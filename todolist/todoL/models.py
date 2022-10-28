77510ae75f5fc8cc73444c64f6b813700cdce007
Dhruvin3103 <dcchawda2003@gmail.com>
2022-10-28 18:21:04 +0530

    just checking how commits work in pycharm

--- todolist/todoL/models.py
+++ todolist/todoL/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class user(AbstractUser):
-#     mob_no = models.CharField(max_length=10,unique=True)
-#     img = models.ImageField(upload_to='todoL/images')
-#     dob = models.DateField(null=True)
+class user(AbstractUser):
+    mob_no = models.CharField(max_length=10,unique=True)
+    img = models.ImageField(upload_to='todoL/images')
+    dob = models.DateField(null=True)
 class Task(models.Model):
     title = models.CharField(max_length=200)
     def __str__(self):
