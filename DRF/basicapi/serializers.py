caf98bd6646196d25e16fbb5b8c97f6ddbb78f82
Dhruvin3103 <dcchawda2003@gmail.com>
2022-11-18 10:27:30 +0530

    use modelserialzer

--- DRF/basicapi/serializers.py
+++ DRF/basicapi/serializers.py
@@ -2,17 +2,8 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import article
 
-class articleserializer(serializers.Serializers):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    date = serializers.DateTimeField()
 
-    def create(self, validated_data):
-        return article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.author = validated_data.get('author', instance.author)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+class articleserializer(serializers.ModelSerializer):
+    class Meta:
+        model = article
+        fields = "__all__"
