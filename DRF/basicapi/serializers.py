64f41260e294e71edc0068712b596e2a87ad339e
Dhruvin3103 <dcchawda2003@gmail.com>
2022-11-18 09:08:31 +0530

    just commit

---
+++ DRF/basicapi/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import routers, serializers, viewsets
+from .models import article
+
+class articleserializer(serializers.Serializers):
+    title = serializers.CharField(max_length=100)
+    author = serializers.CharField(max_length=100)
+    date = serializers.DateTimeField()
+    
+    def create(self, validated_data):
+        return article.objects.create(validated_data)
+    
+    def update(self,instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.date = validated_data.get('date', instance.date)
+        instance.save()
+        return instance
