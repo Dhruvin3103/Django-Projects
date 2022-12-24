7bd3396ca90491ddad62ed8ba694cd663af01be3
Dhruvin3103 <dcchawda2003@gmail.com>
2022-12-24 14:35:07 +0530

    adding models going on

--- DRF/movie/models.py
+++ DRF/movie/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from theatre.models import theatre
+
+
 # Create your models here.
+
+class movie(models.Model):
+    movie_name = models.CharField(max_length=100)
+    movie_no = models.CharField(max_length=100,unique=True)
+    movie_time = models.DateTimeField()
+    movie_lang = models.CharField(max_length=50)# lang includes english, hindi, anime ....
+    movie_type = models.CharField(max_length=50)# type includes 2D,3D,imax ...
+    movie_rating = models.IntegerField(default=17)# minimum age required to watch movie
+    movie_hrs = models.TimeField()# total watchtime of movie
+    movie_ticket_price = models.IntegerField(default=100)
+    theatre_no = models.ForeignKey(theatre , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie_name
--- DRF/theatre/models.py
+++ DRF/theatre/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class theatre(models.Model):
+    theatre_name = models.CharField(max_length=100)
+    theatre_no = models.CharField(max_length=100,unique=True)
+    theatre_add = models.TextField(max_length=1000)
+    theatre_capacity = models.IntegerField(default=50)  # minimum age required to watch movie
+    no_of_theatre = models.TimeField()  # total watchtime of movie
+
+    def __str__(self):
+        return self.theatre_name
